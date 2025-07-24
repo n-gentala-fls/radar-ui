6bed7bd156544b63f71b8c685425f00b9cad8ac7
Nathan Gentala <nathan.gentala@gmail.com>
2025-07-23 20:50:41 -0700

    Initial ui with redirect logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Radar from './components/Radar'; 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/radar/*" exact element={<Radar />} />
+        <Route path="/" exact element={<Navigate to="/radar" replace />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Radar.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useRef, useEffect} from "react"
+import { Box, Container, Grid, Typography } from "@mui/material"
+
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+const Radar = () => {
+    return (
+        <>
+            <AppBar position="static" sx={{ backgroundColor: '#101012', color: 'white' }}>
+                <Toolbar>
+                    <IconButton edge="start" aria-label="menu" sx={{ mr: 2, color: "#00FF99" }}>
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{ color: "#00FF99", flexGrow: 1 }}>
+                        GEODOME
+                    </Typography>
+                    <Button sx={{color: "#00FF99"}}>Stuff...</Button>
+                </Toolbar>
+            </AppBar>
+            
+            <Box
+                backgroundColor="#0A0A0A"
+                display="flex"
+                minHeight="100vh"
+                width="100vw"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <Box
+                    display="flex"
+                    flexDirection="column"
+                    height="300px"
+                    width="400px"
+                    justifyContent="center"
+                    backgroundColor="#1A1F1F"
+                    borderRadius="12px"
+                    boxShadow={5}
+                >
+                    <Typography variant="h4" align="center" gutterBottom>
+                        MAP:
+                    </Typography>
+                </Box>
+            </Box>
+        </>
+    );
+}
+
+export default Radar
